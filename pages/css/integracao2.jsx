5976e17d662302e9be5b758602ec22f81a0c4557
Vinicius Mendes <viniciusmendesrj>
2022-02-28 12:36:28 -0300

    Nova alteração

---
+++ pages/css/integracao2.jsx
@@ -0,0 +1,19 @@
+import styles from "./integracao2.module.css"
+
+export default function integracao2(){
+    return(
+        <div>
+            <div className={styles.vermelha}>
+                <h1>Vermelho</h1>
+            </div>
+
+            <div className={styles.azul}>
+                <h1>Vermelho</h1>
+            </div>
+
+            <div className={styles.branca}>
+                <h1>Vermelho</h1>
+            </div>
+        </div>
+    )
+}
